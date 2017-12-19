04bfa5dd07090f61411eb167a50ef707397dacd8
calebharnell <calebharnell@hotmail.com>
2017-12-19 17:31:24 +1000

    Basic Routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import Home from './components/Home';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+import BookClasses from './components/BookClasses';
+import Gallery from './components/Gallery';
+import Contact from './components/Contact';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+           <div>
+             <ul>
+               <li><Link to="/">Home</Link></li>
+               <li><Link to="/sign-up">Sign Up</Link></li>
+               <li><Link to="/login">Login</Link></li>
+               <li><Link to="/book-classes">Book Classes</Link></li>
+               <li><Link to="/Gallery">Gallery</Link></li>
+               <li><Link to="/Contact">Contact</Link></li>
+             </ul>
+
+             <hr/>
+
+             <Route exact path="/" component={Home}/>
+             <Route path="/sign-up" component={SignUp}/>
+             <Route path="/login" component={Login}/>
+             <Route path="/book-classes" component={BookClasses}/>
+             <Route path="/gallery" component={Gallery}/>
+             <Route path="/contact" component={Contact}/>
+
+           </div>
+         </Router>
       </div>
     );
   }
