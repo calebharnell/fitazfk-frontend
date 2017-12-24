0caaf3977b1612d2385fdb50bd20d571f88906fd
calebharnell <calebharnell@hotmail.com>
2017-12-24 17:10:02 +1000

    Implement react-instagram-embed

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,92 @@
 import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react';
+import InstagramEmbed from 'react-instagram-embed';
 
 class Gallery extends Component {
   render() {
     return (
       <div>
         <h1>Gallery</h1>
+        <Grid container columns={3} doubling stackable>
+          <Grid.Column>
+              <InstagramEmbed
+                url='https://instagr.am/p/Bb-aTR7Hw5D/'
+                maxWidth={400}
+                hideCaption={true}
+                containerTagName='div'
+                protocol=''
+                onLoading={() => {}}
+                onSuccess={() => {}}
+                onAfterRender={() => {}}
+                onFailure={() => {}}
+              />
+          </Grid.Column>
+          <Grid.Column>
+            <InstagramEmbed
+              url='https://instagr.am/p/Ba8fqApHaw-/'
+              maxWidth={400}
+              hideCaption={true}
+              containerTagName='div'
+              protocol=''
+              onLoading={() => {}}
+              onSuccess={() => {}}
+              onAfterRender={() => {}}
+              onFailure={() => {}}
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <InstagramEmbed
+              url='https://instagr.am/p/Ba9wMn9nibr/'
+              maxWidth={400}
+              hideCaption={true}
+              containerTagName='div'
+              protocol=''
+              onLoading={() => {}}
+              onSuccess={() => {}}
+              onAfterRender={() => {}}
+              onFailure={() => {}}
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <InstagramEmbed
+              url='https://instagr.am/p/Ba76QE5n_ci/'
+              maxWidth={400}
+              hideCaption={true}
+              containerTagName='div'
+              protocol=''
+              onLoading={() => {}}
+              onSuccess={() => {}}
+              onAfterRender={() => {}}
+              onFailure={() => {}}
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <InstagramEmbed
+              url='https://instagr.am/p/Ba9wMn9nibr/'
+              maxWidth={400}
+              hideCaption={true}
+              containerTagName='div'
+              protocol=''
+              onLoading={() => {}}
+              onSuccess={() => {}}
+              onAfterRender={() => {}}
+              onFailure={() => {}}
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <InstagramEmbed
+              url='https://instagr.am/p/Ba3bfKVncw7/'
+              maxWidth={400}
+              hideCaption={true}
+              containerTagName='div'
+              protocol=''
+              onLoading={() => {}}
+              onSuccess={() => {}}
+              onAfterRender={() => {}}
+              onFailure={() => {}}
+            />
+          </Grid.Column>
+        </Grid>
       </div>
     );
   }
