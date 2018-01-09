7fd307e49aef6023639e69d36c284264a01c46d7
philip fraser <pr.fraser3@gmail.com>
2018-01-09 10:12:37 +1000

    added registration form, included tokens and added state to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
-import {Menu, Segment} from 'semantic-ui-react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Menu, Segment } from 'semantic-ui-react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { setJwt } from '../api/init';
 import Home from './Home';
 import SignUp from './SignUp';
 import Login from './Login';
@@ -11,35 +12,73 @@ import Logo from './Logo';
 
 class NavBar extends Component {
   state = {
-    activeItem: 'home'
+    activeItem: 'home',
+    loggedIn: null,
+    register: false
   }
+
   handleItemClick = (e, {name}) => this.setState({activeItem: name})
+
+  handleLogOut = () => {
+    this.setState({
+      loggedIn: null
+    })
+    localStorage.removeItem('token')
+  }
+
+  toggleRegister = () => {
+    this.setState({
+      register: !this.state.register
+    })
+  }
+
+  handleLoginResponse = (response) => {
+    this.setState({
+      loggedIn: response.data.token,
+      register: false
+    })
+  }
 
   render() {
     const {activeItem} = this.state
 
-    return (<Router>
-      <div className="NavBar">
-        <Segment className="navbar">
-          <Logo />
-          <Menu secondary="secondary">
-            <Menu.Item as={Link} to='/' name='home' active={activeItem === 'home'} onClick={this.handleItemClick}/>
-            <Menu.Item as={Link} to='/sign-up' name='sign-up' active={activeItem === 'sign-up'} onClick={this.handleItemClick}/>
-            <Menu.Item as={Link} to='/login' name='login' active={activeItem === 'login'} onClick={this.handleItemClick}/>
-            <Menu.Item as={Link} to='/book-classes' name='book-classes' active={activeItem === 'book-classes'} onClick={this.handleItemClick}/>
-            <Menu.Item as={Link} to='/gallery' name='gallery' active={activeItem === 'gallery'} onClick={this.handleItemClick}/>
-            <Menu.Item as={Link} to='/contact' name='contact' active={activeItem === 'contact'} onClick={this.handleItemClick}/>
-          </Menu>
-        </Segment>
+    return (
+      <Router>
+        <div className="NavBar">
+          <Segment className="navbar">
+            <Logo />
+            <Menu secondary="secondary">
+              <Menu.Item as={Link} to='/' name='home' active={activeItem === 'home'} onClick={this.handleItemClick}/>
+              <Menu.Item as={Link} to='/sign-up' name='sign-up' active={activeItem === 'sign-up'} onClick={this.handleItemClick}/>
+              <Menu.Item as={Link} to='/login' name='login' active={activeItem === 'login'} onClick={this.handleItemClick}/>
+              <Menu.Item as={Link} to='/book-classes' name='book-classes' active={activeItem === 'book-classes'} onClick={this.handleItemClick}/>
+              <Menu.Item as={Link} to='/gallery' name='gallery' active={activeItem === 'gallery'} onClick={this.handleItemClick}/>
+              <Menu.Item as={Link} to='/contact' name='contact' active={activeItem === 'contact'} onClick={this.handleItemClick}/>
+            </Menu>
+          </Segment>
 
-        <Route exact="exact" path="/" component={Home}/>
-        <Route path="/sign-up" component={SignUp}/>
-        <Route path="/login" component={Login}/>
-        <Route path="/book-classes" component={BookClasses}/>
-        <Route path="/gallery" component={Gallery}/>
-        <Route path="/contact" component={Contact}/>
-      </div>
-    </Router>)
+          <Route exact="exact" path="/" component={Home}/>
+          <Route
+            path="/sign-up"
+            render={(routeProps) => (
+              <SignUp {...routeProps} handleLoginResponse={this.handleLoginResponse} />
+            )}
+          />
+          <Route path="/login" component={Login}/>
+          <Route path="/book-classes" component={BookClasses}/>
+          <Route path="/gallery" component={Gallery}/>
+          <Route path="/contact" component={Contact}/>
+        </div>
+      </Router>
+    )
+  }
+
+  componentDidMount() {
+    let token = localStorage.getItem('token')
+    token && setJwt(token) 
+    this.setState({
+      loggedIn: token
+    })
   }
 }
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,11 +1,72 @@
 import React, { Component } from 'react';
+import { Grid, Button, Form } from 'semantic-ui-react'
+import { api, setJwt } from '../api/init';
 
 class SignUp extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.passwordValue === this.state.confirmPasswordValue){
+      api.post('/auth/register', {
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then((response) => {
+      	console.log(response)
+        setJwt(response.data.token)
+        this.props.handleLoginResponse(response)
+      })
+      .catch((error) => {
+        console.log('An error occured when trying to register.', error)
+      })
+    } else {
+      alert('Your password fields do not match.')
+    }
+  }
   render() {
+  	const { firstName, lastName, email, password, confirmPassword } = this.state
+
     return (
-      <div>
-        <h1>Sign Up</h1>
-      </div>
+    	<Grid centered columns={2}>
+	    	<Grid.Column>
+		      <Form className="signup-form" onSubmit={this.handleSubmit}>
+		      	<Form.Group>
+		          <Form.Input label='Email' placeholder='Email' name='email' value={email} onChange={this.handleChange} width={16} />
+		        </Form.Group>  
+		        <Form.Group>
+		          <Form.Input label='First Name' placeholder='First Name' name='firstName' value={firstName} onChange={this.handleChange} width={16} />
+		        </Form.Group>
+		        <Form.Group>
+		          <Form.Input label='Last Name' placeholder='Last Name' name='lastName' value={lastName} onChange={this.handleChange} width={16} />
+		        </Form.Group>
+		        <Form.Group>
+		          <Form.Input label='Password' placeholder='Password' name='password' value={password} onChange={this.handleChange} width={16} />
+		        </Form.Group>
+		        <Form.Group>
+		          <Form.Input label='Confirm Password' placeholder='Confirm Password' name='confirmPassword' value={confirmPassword} onChange={this.handleChange} width={16} />
+		        </Form.Group>  
+		        <Button type='submit'>Submit</Button>
+		      </Form>
+	      </Grid.Column>
+      </Grid>
     );
   }
 }
