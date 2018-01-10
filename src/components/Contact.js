0fbefa4b0a573b484631285fe9f8f44a8559124c
calebharnell <calebharnell@hotmail.com>
2018-01-10 11:47:28 +1000

    Add google map

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import MapsEmbed from './MapsEmbed';
 
 class Contact extends Component {
   render() {
     return (
       <div>
         <h1>Contact</h1>
+        <MapsEmbed />
       </div>
     );
   }
