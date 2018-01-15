09a8bb82dec3701b800e19a64813b830ef7ccda0
philip fraser <pr.fraser3@gmail.com>
2018-01-15 11:24:42 +1000

    Setup activate/deactivate users. Added an AdminUserRow component for the Users table, added a Button for activation/deactivation of each user

---
+++ src/components/ActiveButton.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react'
+
+const ActiveButton = ({ active, handleActive }) => (
+	<Button onClick={handleActive}>{active ? 'Deactivate' : 'Activate'}</Button>
+)
+
+export default ActiveButton;
