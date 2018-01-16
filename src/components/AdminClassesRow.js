1ef2900073fcd9a223bb1e90638a2d33e9a6b8fc
digitalbinge <dave.barr@outlook.com>
2018-01-16 16:09:26 +1000

    create and edit complete

--- src/components/AdminClassesRow.js
+++ src/components/AdminClassesRow.js
@@ -1,32 +1,53 @@
 import React, { Component } from 'react';
 import { api, setJwt } from '../api/init';
 import { Table, Button } from 'semantic-ui-react'
+import CreateClassModal from './CreateClassModal';
 
 class AdminClassesRow extends Component {
 	constructor(props){
 	  super(props)
 	  this.state = {
-	    sessions: this.props.sessions,
-	    editable: false
+	  	createModalActive: false
 	  }
 	}
 
-	handleEditSession = () => {
-		this.setState({
-			editable: true
-		})
+	handleCreateModalOpen = () => {
+	  this.setState(prevState => ({
+	    createModalActive: true
+	  }))
+	}
+
+	handleCreateModalCancel = () => {
+	  this.setState(prevState => ({
+	    createModalActive: false
+	  }))
+	}
+
+	handleSave = (formData) => {
+		this.handleCreateModalCancel()
+		this.props.onUpdate(formData)
 	}
 
   render() {
 		return (			
 			<Table.Body>
 				<Table.Row>
-				  	<Table.Cell contentEditable={this.state.editable}>{this.props.day}</Table.Cell>
-				  	<Table.Cell contentEditable={this.state.editable}>{this.props.time}</Table.Cell>
-				  	<Table.Cell contentEditable={this.state.editable}>{this.props.name}</Table.Cell>
-				  	<Table.Cell contentEditable={this.state.editable}>{this.props.instructor}</Table.Cell>
-				  	<Table.Cell><Button onClick={this.handleEditSession}>Edit</Button></Table.Cell>
+				  	<Table.Cell>{this.props.day}</Table.Cell>
+				  	<Table.Cell>{this.props.time}</Table.Cell>
+				  	<Table.Cell>{this.props.name}</Table.Cell>
+				  	<Table.Cell>{this.props.instructor}</Table.Cell>
+				  	<Table.Cell>
+				  		<Button onClick={this.handleCreateModalOpen}>Edit</Button>
+				  	</Table.Cell>
 				</Table.Row>
+				{this.state.createModalActive && <CreateClassModal
+				  onCancel={this.handleCreateModalCancel}
+				  onSave={this.handleSave}
+				  day={this.props.day}
+				  time={this.props.time}
+				  name={this.props.name}
+				  instructor={this.props.instructor}
+				  />}
 			</Table.Body>
 		)
 	}
