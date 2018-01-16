e069af01aa1cd3cd9ccd8088d0b105c693289d26
calebharnell <calebharnell@hotmail.com>
2018-01-16 22:33:38 +1000

    Add Footer with social icons

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Segment, Icon } from 'semantic-ui-react'
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <Segment>
+          <h3>Connect with us on social media!</h3>
+          <Icon name='facebook square' size='huge' link onClick={() => window.open('https://www.facebook.com/Fitazfk/', '_blank')}/>
+          <Icon name='instagram' size='huge' link onClick={() => window.open('https://www.instagram.com/fitazfk/', '_blank')}/>
+          <Icon name='youtube square' size='huge' link onClick={() => window.open('https://www.youtube.com/channel/UC_3G99GSLJlRBhhR8Gs5hTw', '_blank')}/>
+          <Icon name='pinterest square' size='huge' link onClick={() => window.open('https://au.pinterest.com/fitazfk/', '_blank')}/>
+          <Segment>
+            <p>©2018 Fitaz Health Pty Ltd</p>
+          </Segment>
+        </Segment>
+      </div>
+    )
+  }
+}
+
+export default Footer;
