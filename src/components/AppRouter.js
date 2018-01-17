eea0a98fb9ae8cc9e94c24d33672e3298dad0248
calebharnell <calebharnell@hotmail.com>
2018-01-17 13:51:39 +1000

    Navbar update

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Menu, Segment } from 'semantic-ui-react';
+import { Menu, Segment, Icon } from 'semantic-ui-react';
 import { BrowserRouter as Router, Route, Link, Redirect} from 'react-router-dom';
 import { setJwt } from '../api/init';
 import Home from './Home';
@@ -16,11 +16,20 @@ import Logo from './Logo';
 import jwtDecode from 'jwt-decode';
 
 class AppRouter extends Component {
-  state = {
-    activeItem: 'home',
-    loggedIn: null,
-    currentUser: null,
-    admin: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: 'home',
+      loggedIn: null,
+      currentUser: null,
+      admin: false,
+      isDesktop: true
+    }
+    this.updatePredicate = this.updatePredicate.bind(this);
+  }
+
+  updatePredicate() {
+    this.setState({ isDesktop: window.innerWidth > 1000 });
   }
 
   handleItemClick = (e, {name}) => this.setState({activeItem: name})
@@ -46,93 +55,123 @@ class AppRouter extends Component {
   }
 
   render() {
-    const { activeItem, loggedIn, currentUser } = this.state;
+    const { activeItem, loggedIn, currentUser, isDesktop } = this.state;
 
-    let loggedInButtons = null
-    if (!loggedIn) {
-      loggedInButtons = <Menu secondary stackable>
-                          <Menu.Item as={Link} to='/sign-up' name='sign-up' active={activeItem === 'sign-up'} onClick={this.handleItemClick}/>
-                          <Menu.Item as={Link} to='/login' name='login' active={activeItem === 'login'} onClick={this.handleItemClick}/>
-                        </Menu>
-    } else if (loggedIn && this.state.admin) {
-      loggedInButtons = <Menu secondary stackable>
-                          <Menu.Item as={Link} to='/admin/classes' name='adminClasses' active={activeItem === 'adminClasses'} onClick={this.handleItemClick}/>
-                          <Menu.Item as={Link} to='/admin/users' name='adminUsers' active={activeItem === 'adminUsers'} onClick={this.handleItemClick}/>
-                          <Menu.Item as={Link} to='/account' name='account' active={activeItem === 'account'} onClick={this.handleItemClick}/>
-                          <Menu.Item as={Link} to='/logout' name='logout' onClick={this.handleLogOut}/>
-                        </Menu>
-    } else {
-      loggedInButtons = <Menu secondary stackable>
-                          <Menu.Item as={Link} to='/account' name='account' active={activeItem === 'account'} onClick={this.handleItemClick}/>
-                          <Menu.Item as={Link} to='/login' name='logout' active={activeItem === 'login'} onClick={this.handleLogOut}/>
-                        </Menu>
+    // let loggedInButtons = null
+    // if (!loggedIn) {
+    //   loggedInButtons = <Menu secondary stackable>
+    //                       <Menu.Item as={Link} to='/sign-up' name='sign-up' active={activeItem === 'sign-up'} onClick={this.handleItemClick}/>
+    //                       <Menu.Item as={Link} to='/login' name='login' active={activeItem === 'login'} onClick={this.handleItemClick}/>
+    //                     </Menu>
+    // } else if (loggedIn && this.state.admin) {
+    //   loggedInButtons = <Menu secondary stackable>
+    //                       <Menu.Item as={Link} to='/admin/classes' name='adminClasses' active={activeItem === 'adminClasses'} onClick={this.handleItemClick}/>
+    //                       <Menu.Item as={Link} to='/admin/users' name='adminUsers' active={activeItem === 'adminUsers'} onClick={this.handleItemClick}/>
+                          // <Menu.Item as={Link} to='/account' name='account' active={activeItem === 'account'} onClick={this.handleItemClick}/>
+                          // <Menu.Item as={Link} to='/logout' name='logout' onClick={this.handleLogOut}/>
+    //                     </Menu>
+    // } else {
+    //   loggedInButtons = <Menu secondary stackable>
+    //                       <Menu.Item as={Link} to='/account' name='account' active={activeItem === 'account'} onClick={this.handleItemClick}/>
+    //                       <Menu.Item as={Link} to='/login' name='logout' active={activeItem === 'login'} onClick={this.handleLogOut}/>
+    //                     </Menu>
+    // }
+
+    let responsiveNavbar = null;
+    if (isDesktop && !loggedIn) {
+      responsiveNavbar =  <Menu secondary stackable>
+                            <Menu.Item as={Link} to='/' name='home' active={activeItem === 'home'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/book-classes' name='book-classes' active={activeItem === 'book-classes'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/classes' name='classes' active={activeItem === 'classes'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/gallery' name='gallery' active={activeItem === 'gallery'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/contact' name='contact' active={activeItem === 'contact'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/account' name='account' active={activeItem === 'account'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/login' name='logout' active={activeItem === 'login'} onClick={this.handleLogOut}/>
+                          </Menu>
+    } else if (isDesktop && loggedIn) {
+      responsiveNavbar =  <Menu secondary stackable>
+                            <Menu.Item as={Link} to='/' name='home' active={activeItem === 'home'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/book-classes' name='book-classes' active={activeItem === 'book-classes'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/classes' name='classes' active={activeItem === 'classes'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/gallery' name='gallery' active={activeItem === 'gallery'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/contact' name='contact' active={activeItem === 'contact'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/account' name='account' active={activeItem === 'account'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/logout' name='logout' onClick={this.handleLogOut}/>
+                          </Menu>
+    } else if (isDesktop && loggedIn && this.state.admin) {
+      responsiveNavbar =  <Menu secondary stackable>
+                            <Menu.Item as={Link} to='/' name='home' active={activeItem === 'home'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/book-classes' name='book-classes' active={activeItem === 'book-classes'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/classes' name='classes' active={activeItem === 'classes'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/gallery' name='gallery' active={activeItem === 'gallery'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/contact' name='contact' active={activeItem === 'contact'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/admin/classes' name='adminClasses' active={activeItem === 'adminClasses'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/admin/users' name='adminUsers' active={activeItem === 'adminUsers'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/account' name='account' active={activeItem === 'account'} onClick={this.handleItemClick}/>
+                            <Menu.Item as={Link} to='/logout' name='logout' onClick={this.handleLogOut}/>
+                          </Menu>
     }
 
     return (
-      <Router>
-        <div className="NavBar">
-          <Segment className="navbar">
-            <Menu secondary stackable>
-              <Menu.Item>
-                <Logo />
-              </Menu.Item>
-              <Menu.Item as={Link} to='/' name='home' active={activeItem === 'home'} onClick={this.handleItemClick}/>
-              <Menu.Item as={Link} to='/book-classes' name='book-classes' active={activeItem === 'book-classes'} onClick={this.handleItemClick}/>
-              <Menu.Item as={Link} to='/classes' name='classes' active={activeItem === 'classes'} onClick={this.handleItemClick}/>
-              <Menu.Item as={Link} to='/gallery' name='gallery' active={activeItem === 'gallery'} onClick={this.handleItemClick}/>
-              <Menu.Item as={Link} to='/contact' name='contact' active={activeItem === 'contact'} onClick={this.handleItemClick}/>
-              {loggedInButtons}
-            </Menu>
-          </Segment>
+        <Router>
+          <div className="NavBar">
+            <Segment className="navbar">
+              <Menu secondary fluid>
+                <Menu.Item className='logo-nav'>
+                  <Logo />
+                </Menu.Item>
+                {responsiveNavbar}
+              </Menu>
+            </Segment>
 
-          <Route exact path="/" component={Home}/>
-          <Route
-            path="/sign-up"
-            render={(routeProps) => (
-              <SignUp {...routeProps} handleLoginResponse={this.handleLoginResponse} />
-            )}
-          />
-          <Route
-            path="/login"
-            render={(routeProps) => (
-              <Login {...routeProps} handleLoginResponse={this.handleLoginResponse} handleSetAdmin={this.handleSetAdmin} />
-            )}
-          />
-          <Route
-            path="/book-classes"
-            render={(routeProps) => (
-              <BookClasses {...routeProps} currentUser={currentUser} />
-            )}
-          />
-          <Route path="/classes" component={Classes}/>
-          <Route path="/gallery" component={Gallery}/>
-          <Route path="/contact" component={Contact}/>
-          <Route path="/admin/classes" component={AdminClasses}/>
-          <Route path="/admin/users" component={AdminUsers}/>
-          <Route
-            path="/account"
-            render={(routeProps) => (
-              <Account {...routeProps} currentUser={currentUser} />
-            )}
-          />
-          <Route exact path="/logout" render={() => (
-            <Redirect to="/login"/>
-            )}/>
-        </div>
-      </Router>
+            <Route exact path="/" component={Home}/>
+            <Route
+              path="/sign-up"
+              render={(routeProps) => (
+                <SignUp {...routeProps} handleLoginResponse={this.handleLoginResponse} />
+              )}
+            />
+            <Route
+              path="/login"
+              render={(routeProps) => (
+                <Login {...routeProps} handleLoginResponse={this.handleLoginResponse} handleSetAdmin={this.handleSetAdmin} />
+              )}
+            />
+            <Route
+              path="/book-classes"
+              render={(routeProps) => (
+                <BookClasses {...routeProps} currentUser={currentUser} />
+              )}
+            />
+            <Route path="/classes" component={Classes}/>
+            <Route path="/gallery" component={Gallery}/>
+            <Route path="/contact" component={Contact}/>
+            <Route path="/admin/classes" component={AdminClasses}/>
+            <Route path="/admin/users" component={AdminUsers}/>
+            <Route
+              path="/account"
+              render={(routeProps) => (
+                <Account {...routeProps} currentUser={currentUser} />
+              )}
+            />
+            <Route exact path="/logout" render={() => (
+              <Redirect to="/login"/>
+              )}/>
+          </div>
+        </Router>
     )
   }
 
   componentDidMount() {
+    this.updatePredicate();
+    window.addEventListener("resize", this.updatePredicate);
     let token = localStorage.getItem('token')
-
     if (!token) {
       return;
     }
-    
     token && setJwt(token)
-
     const decodedToken = jwtDecode(token)
+    console.log(decodedToken.role)
     if (isTokenExpired(decodedToken.exp)) {
       this.handleLogOut()
     } else {
@@ -142,7 +181,13 @@ class AppRouter extends Component {
         admin: decodedToken.role || false
       })
     }
+
   }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updatePredicate);
+  }
+
 }
 
 export default AppRouter;
